cfa39b3d4b926d85c9c49019cee0bd8b9a2e84f0
Leonardo Rocha Andreazzi <zarp_landreazzi@orizon.com.br>
2021-02-25 16:13:11 -0300

    first commit

---
+++ src/main/java/br/com/alura/mvc/mudi/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package br.com.alura.mvc.mudi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.models.Pedido;
+import br.com.alura.mvc.mudi.repositories.PedidoRepository;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		
+		model.addAttribute("pedidos", pedidos);
+		
+		return "home";
+	}
+}
