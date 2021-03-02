201fc59845e206b8dac1bc463aa04f56fd92a353
Leonardo Rocha Andreazzi <zarp_landreazzi@orizon.com.br>
2021-03-02 08:23:05 -0300

    usando Bean Validation para validar o DTO

--- src/main/java/br/com/alura/mvc/mudi/controllers/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controllers/PedidoController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,15 @@ public class PedidoController {
 	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(NovoPedidoDto novoPedidoDto) {
 		return "pedido/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(NovoPedidoDto novoPedidoDto) {
+	public String novo(@Valid NovoPedidoDto novoPedidoDto, BindingResult result) {
+		
+		if(result.hasErrors())
+			return "pedido/formulario";
 		
 		Pedido pedido = novoPedidoDto.toPedido();
 		
